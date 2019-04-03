dd2c7428eabd35557732544712c98ff47cf95de8
Kevin <kevin-gillet@sfr.fr>
2019-04-03 11:58:49 +0200

    Intégration des variables

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-#First algorithm
+"""
+First algorithm
+@version 0.0.2
+Add two operand and replace compute method
+"""
 resultat = 0 #Definition variable
-resultat = 2 + 3
+operande1 = 3
+operande2 = 2
+resultat = operande1 + operande2
 print(resultat)
